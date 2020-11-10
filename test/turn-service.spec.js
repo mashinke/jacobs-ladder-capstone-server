f2aa613fbcd308d9051e41d353f863dc7dada0a2
Marc Trius <derpayatz@gmail.com>
2020-11-10 12:23:42 -0600

    prepare testing

---
+++ test/turn-service.spec.js
@@ -0,0 +1,29 @@
+const TurnService = require('../src/game/turn-service');
+const TestHelpers = require('./test-helpers');
+const knex = require('knex');
+const { expect } = require('chai');
+const testHelpers = require('./test-helpers');
+
+describe('Game service object', () => {
+  let db;
+  const testUsers = TestHelpers.createTestUsers();
+  const testGames = TestHelpers.createTestGames();
+  const testCards = TestHelpers.createTestCards();
+  const testQuestions = TestHelpers.createTestQuestions();
+  const testAnswers = testHelpers.createTestAnswers();
+  before('establish db connection', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DATABASE_URL
+    });
+  });
+  before('ensure test db is empty', () => {
+    return db.raw('truncate game, app_user, turn, card, question, answer restart identity cascade');
+  });
+  after('destroy db connection', () => {
+    return db.destroy();
+  });
+  afterEach('clear db data', () => {
+    return db.raw('truncate game, app_user, turn, card, question, answer restart identity cascade');
+  });
+});
