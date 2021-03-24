b3fda053eefd3d0b9bcce359c501db17e53c9cbd
Masha Trius <triusmaria@gmail.com>
2021-03-24 15:43:35 -0500

    require ssl in pg connection

--- src/server.js
+++ src/server.js
@@ -1,21 +1,28 @@
 require('dotenv').config();
+const parse = require('pg-connection-string').parse;
 const knex = require('knex');
 
 const { NODE_ENV, PORT, DB_CONNECTION } = require('./config');
 const app = require('./app');
+
+const pgconfig = typeof DB_CONNECTION === 'string'
+  ? parse(DB_CONNECTION)
+  : DB_CONNECTION;
+
+pgconfig.ssl = { rejectUnauthorized: false };
 
 app.set(
   'db',
   knex(
     {
       client: 'pg',
-      connection: DB_CONNECTION
+      connection: pgconfig,
     }
   )
 );
 
 console.log(DB_CONNECTION);
-app.get('db').from('game').select('*').then(res => console.log()).catch(err => console.log(err));
+app.get('db').from('game').select('*').then(res => console.log(res)).catch(err => console.log(err));
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Server started in ${NODE_ENV} mode on ${PORT}...`));
